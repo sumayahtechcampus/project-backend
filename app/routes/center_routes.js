fec7ac55bebc6bfd23a4ff3611f55f0812527b29
sumayahtechcampus <sumayah.ali.saleh@gmail.com>
2019-12-15 15:44:54 +0300

    add seed file

--- app/routes/center_routes.js
+++ app/routes/center_routes.js
@@ -2,42 +2,31 @@
 const express = require('express')
 // Passport docs: http://www.passportjs.org/docs/
 const passport = require('passport')
-
 // pull in Mongoose model for parents
 const Center = require('../models/center')
-
-
 const customErrors = require('../../lib/custom_errors')
-
 // we'll use this function to send 404 when non-existant document is requested
 const handle404 = customErrors.handle404
 // we'll use this function to send 401 when a user tries to modify a resource
 // that's owned by someone else
 const requireOwnership = customErrors.requireOwnership
-
 const removeBlanks = require('../../lib/remove_blank_fields')
 // passing this as a second argument to `router.<verb>` will make it
 // so that a token MUST be passed for that route to be available
 // it will also set `req.user`
 const requireToken = passport.authenticate('bearer', { session: false })
-
 // instantiate a router (mini app that only handles routes)
 const router = express.Router()
-
 // INDEX
 // GET /centers
-router.get('/centers', requireToken, (req, res, next) => {
-  
-  Center.find({owner: req.user.id})
+router.get('/centers',  (req, res, next) => {
+  Center.find(req.body.center)
     .then(centers => res.status(200).json({centers: centers}))
     .catch(next)
-  
-
 })
-
 // SHOW
 // GET /centers/5a7db6c74d55bc51bdf39793
-router.get('/centers/:id', requireToken, (req, res, next) => {
+router.get('/centers/:id', (req, res, next) => {
   // req.params.id will be set based on the `:id` in the route
   Center.findById(req.params.id)
     .then(handle404)
@@ -46,40 +35,32 @@ router.get('/centers/:id', requireToken, (req, res, next) => {
       // pass the `req` object and the Mongoose record to `requireOwnership`
       // it will throw an error if the current user isn't the owner
       requireOwnership(req, parent)
-    
       res.status(200).json({ center: center.toObject() })
     })
     // if an error occurs, pass it to the handler
     .catch(next)
 })
-
 // CREATE
 // POST /center
-router.post('/centers', requireToken, (req, res, next) => {
-  req.body.center.owner = req.user.id
-
+router.post('/centers', (req, res, next) => {
+ // req.body.center.owner = req.user.id
   Center.create(req.body.center)
     // respond to succesful `create` with status 201 and JSON of new "parent"
     .then(center => {
       res.status(201).json({ center: center.toObject() })
     })
-    
     .catch(next)
 })
-
 // UPDATE
 // PATCH /centers/5a7db6c74d55bc51bdf39793
-router.patch('/centers/:id', requireToken, removeBlanks, (req, res, next) => {
+router.patch('/centers/:id', removeBlanks, (req, res, next) => {
   // if the client attempts to change the `owner` property by including a new
   // owner, prevent that by deleting that key/value pair
   delete req.body.center.owner
-
   Center.findById(req.params.id)
     .then(handle404)
     .then(center => {
-      
       requireOwnership(req, center)
-
       // pass the result of Mongoose's `.update` to the next `.then`
       return center.update(req.body.center)
     })
@@ -88,10 +69,9 @@ router.patch('/centers/:id', requireToken, removeBlanks, (req, res, next) => {
     // if an error occurs, pass it to the handler
     .catch(next)
 })
-
 // DESTROY
 // DELETE /centers/5a7db6c74d55bc51bdf39793
-router.delete('/centers/:id', requireToken, (req, res, next) => {
+router.delete('/centers/:id', (req, res, next) => {
   Center.findById(req.params.id)
     .then(handle404)
     .then(center => {
@@ -103,5 +83,4 @@ router.delete('/centers/:id', requireToken, (req, res, next) => {
     // if an error occurs, pass it to the handler
     .catch(next)
 })
-
 module.exports = router
---
+++ seed.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Center = require('./app/models/center');
+
+
+const mongoURI = 'mongodb://localhost/mongoRelationships';
+mongoose.connect(mongoURI, { useNewUrlParser: true }, () => {
+  console.log('the connection with mongod is established')
+});
+
+const centerActivity = [
+    {
+        name: 'The Learning Zone',
+        category: ' FUN & LEARN',
+        description: ' We are a vibrant and evolving children’s center located in Riyadh.  Offering a bundle of world renowned Children Programs, with a verity of services ranging from afterschool activities and summer camps, to birthday parties. We support each child’s journey toward his or her social, emotional, cognitive, physical, and aesthetic potential. We continue to improve our services by collaborating with other.',
+    },
+    {
+        name: 'My Gym',
+        category: 'FUN & LEARN',
+        description: 'My Gym was born out of our passion to teach children in a fun, physically engaging environment. We delighted in seeing childrens eyes light up for all those incredible milestones, from the tentative first steps to tumbling down soft mats. Sharing the parents’ excitement at their children’s accomplishments was the high point of our day',
+    },
+    {
+        name: 'Little explorers',
+        category: 'FUN & LEARN',
+        description: 'Little Explorers has three unique main areas that helps children develop their sense of individuality and social understanding, learn about spatial and time-related awareness, excite their thirst for knowledge, build their understanding of science and technology and burn off some energy through exercise!',
+    },
+];
+
+// Center.insertMany(centerActivity, (err, centers)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log(centers);
+//     }
+// });
