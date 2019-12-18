33ddb552760926a5fb4acd7310b9e00c76cd93b8
sumayahtechcampus <sumayah.ali.saleh@gmail.com>
2019-12-18 15:28:04 +0300

    Fix Children to Childrens Router

--- app/routes/children_routes.js
+++ app/routes/children_routes.js
@@ -1,27 +1,19 @@
 const express = require('express')
-
 const passport = require('passport')
-
 const Children = require('../models/children')
-
 const customErrors = require('../../lib/custom_errors')
-
 const handle404 = customErrors.handle404
-
 const requireOwnership = customErrors.requireOwnership
-
 const removeBlanks = require('../../lib/remove_blank_fields')
-
 const requireToken = passport.authenticate('bearer', { session: false })
-
 const router = express.Router()
 
 
 
-router.get('/children',requireToken,(req, res, next) => {
+router.get('/childrens',requireToken,(req, res, next) => {
   
-  Children.find({owner: req.user.id})
-    .then(children => res.status(200).json({children: children}))
+  Children.find()
+    .then(children => res.status(200).json({childrens: children}))
     .catch(next)
   
   
